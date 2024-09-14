5e6497cc7474e0b3007ddbfbf1b9f14dbf586136
chaudharysagar <sagarchaudharynn06@gmail.com>
2024-09-15 00:40:17 +0530

    Login page added

---
+++ src/pages/chat/Chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Chat.css'
+
+const Chat = () => {
+  return (
+    <div>
+      This is chat interface
+    </div>
+  )
+}
+
+export default Chat
---
+++ src/pages/profileUpdate/ProfileUpdate.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProfileUpdate'
+
+const ProfileUpdate = () => {
+  return (
+    <div>
+      profile section
+    </div>
+  )
+}
+
+export default ProfileUpdate
